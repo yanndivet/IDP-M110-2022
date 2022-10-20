908506c0b22506c692bdf91853a38250b91793b5
Kaaasttuk <88781164+Kaaasttuk@users.noreply.github.com>
2022-10-21 05:04:07 +0900

    Update main_navigation.ino

--- main_navigation.ino
+++ main_navigation.ino
@@ -12,6 +12,8 @@ int right_counter = 0;
 bool hold_block = false;
 bool magnetic;
 double distance_thresh = 5.0;
+int change_counter_RRS = 0;
+int color_RRS = 1;         
 
 Servo myservo;
 
@@ -42,20 +44,24 @@ void loop() {
   // Put your main code here, to run repeatedly:
 // ---------------------------------- LINE FOLLOWING ----------------------------------
 // Move forward
-  if((analogRead(LS) < threshold) && (analogRead(RS) < threshold)) {
-    move_forward();
+  if(digitalRead(LS) && digitalRead(RS)) {
+     move_forward();
 
-    // Checks if most right sensor is over white. Could also include most left sensor to make sure, but not necessary
-    if ((analogRead(RS) > threshold)) {
-      right_counter++;
-      // Turn right at second intersection
-      if (right_counter == 2) {
+    // For initial movement: use counter to determine when to turn right
+    if ((digitalRead(RRS)) != color_RRS) {
+      color_RRS = digitalRead(RRS);
+      change_counter_RRS ++;
+      Serial.println(change_counter_RRS);
+      // Turn right at second intersection (when we detect four colour changes)
+      if (change_counter_RRS == 4) {
+        Serial.println("4 Colour Changes Detected");
         // Turn right using both wheels until right sensor reaches white line
-        while (analogRead(RS) < threshold) {
+        delay(1000);
+        while (digitalRead(LS)) {
           turn_right();
-          }
         }
       }
+    }
   }
 
 // Turn left 
@@ -70,7 +76,7 @@ void loop() {
 
 
 // ---------------------------------- ULTRASOUND AND HALL-EFFECT SENSORS ----------------------------------
-  // for efficiency, maybe we can only start checking distance from ultrasensor after a certain amount of time? 
+  // for efficiency, maybe we can only start checking distance from ultrasensor after a certain amount of time?
   ultrasound_setup();
   // Reads the echoPin, returns the sound wave travel time in microseconds
   long duration = pulseIn(echoPin, HIGH);
@@ -86,18 +92,23 @@ void loop() {
 
 // ---------------------------------- BLOCK COLLECTION AND RETURN ----------------------------------
   if (distance < distance_thresh) {
+    if (hold_block == false){
+      stop();
+      delay(3000);
+    }
+    myservo.write(180); // move servo by 180 degrees for capture
     hold_block = true;
-    myservo.write(180); // move servo by 180 degrees
-    magnetic = magnetic_detection(voltage);
-    if (magnetic == true) {
-      digitalWrite(red, HIGH);
-    }
-    else {
-      digitalWrite(green, HIGH);
-    }
+    // commented out below as electrical team automated LED process
+    // magnetic = magnetic_detection(voltage);
+    // if (magnetic == true) {
+    //   digitalWrite(red, HIGH);
+    // }
+    // else {
+    //   digitalWrite(green, HIGH);
+    // }
   }
 
   if (hold_block == true) {
-    block_drop(magnetic, right_counter);
+    block_drop(magnetic, change_counter_RRS);
   }
 }

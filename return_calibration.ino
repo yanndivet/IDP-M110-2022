afbb1b483d1f37a2027aeddfe9f1ba3d2a8ea964
yanndivet <98603915+yanndivet@users.noreply.github.com>
2022-10-27 23:22:58 +0100

    Add variables + drop block fct (although needs line_following)

--- return_calibration.ino
+++ return_calibration.ino
@@ -6,8 +6,21 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 Adafruit_DCMotor *myMotor_right = AFMS.getMotor(2);
 Adafruit_DCMotor *myMotor_left = AFMS.getMotor(1); // warning this motor is reversed
 
-int motor_speed = 255;
 
+int mov_time;
+void wait(int mov_time = 0);
+void forward(int mov_time = 0);
+void backward(int mov_time = 0);
+void left(int mov_time = 0);
+void right(int mov_time = 0);
+
+
+int motor_speed = 255;
+int time_right_angle = 900;
+int time_back_from_block = 300;
+int time_change_squares = 3000;
+int time_stop = 5*60*1000;
+int time_forward_dropblock = 1000; // needs to be tested to see if accurate
 
 
 bool hold_block = false;
@@ -45,89 +58,80 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   magnetic = false;
+  // block_drop(magnetic, counter_RRSorsmth);
   return_robot(magnetic);
 
 }
 
 // ---------------------- FUNCTIONS DEFINITIONS ----------------------
 
-void wait() {
+void wait(int mov_time) {
   myMotor_right->run(RELEASE);
   myMotor_left->run(RELEASE); 
-}
+  delay(mov_time); }
 
-void forward () {
+void forward (int mov_time) {
   myMotor_right->run(BACKWARD);
-  myMotor_left->run(BACKWARD); 
-}
+  myMotor_left->run(BACKWARD);
+  delay(mov_time); }
 
-void left() {
-  myMotor_right->run(BACKWARD);
-  myMotor_left->run(FORWARD); 
-}
-
-void right() {
-  myMotor_right->run(FORWARD);
-  myMotor_left->run(BACKWARD); 
-}
-
-void backward() {
+void backward (int mov_time) {
   myMotor_right->run(FORWARD);
   myMotor_left->run(FORWARD); 
-}
+  delay(mov_time); }
+
+void left (int mov_time) {
+  myMotor_right->run(BACKWARD);
+  myMotor_left->run(FORWARD); 
+  delay(mov_time); }
+
+void right(int mov_time) {
+  myMotor_right->run(FORWARD);
+  myMotor_left->run(BACKWARD); 
+  delay(mov_time); }
 
 // ---------------------- MORE SOPHISTICATED MOVEMENTS ----------------------
-// void block_drop(bool magnetic, int right_counter) {
-//     // if block is magnetic, drop it in red region
-//     int counter_turn;
-//     // these counter values have to be adjusted experimentally.
-//     if (magnetic == true) {
-//       counter_turn = 11;
-//     }
-//     else {
-//       counter_turn = 15;
-//     }
+void block_drop(bool magnetic, int right_counter) {
+    // if block is magnetic, drop it in red region
+    int counter_turn;
 
-//     if (right_counter == counter_turn) {
-//       // Get inside square region
-//       right(1000);
-//       right_till_line();
-//       forward(3000);
+    if (magnetic == true) {
+      counter_turn = 1;
+    }
+    else {
+      counter_turn = 5;
+    }
 
-//       // drop block
-//       myservo.write(angle_backward);
-//       hold_block = false;
+    if (right_counter == counter_turn) {
+      // Get inside square region
+      right(time_right_angle);
+      forward(time_forward_dropblock);
 
-//       // Get outside square region and back on line
-//       backward(3000);
-//       left(1000);
-//       right_till_line();
-//     }
-// }
+      // drop block
+      // myservo.write(angle_backward);
+      hold_block = false;
+
+      // get out of function, we don't want this repeating
+      right_counter ++;
+
+    }
+}
 
 void return_robot(bool magnetic) {
 
   if (!(magnetic)){
-    backward();
-    delay(300);
-    left();
-    delay(900);
-    forward();
-    delay(3000);
-    wait();
-    delay(100000);
+    backward(time_back_from_block);
+    left(time_right_angle);
+    forward(time_change_squares);
+    wait(time_stop);
   }
 
 
 
   else if (magnetic){
-    backward();
-    delay(300);  
-    right();
-    delay(900);
-    forward();
-    delay(3000);
-    wait();
-    delay(100000);
+    backward(time_back_from_block); 
+    right(time_right_angle);
+    forward(time_change_squares);
+    wait(time_stop);
   }
 }
